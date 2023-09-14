33515ee6edb4137fbb3597d917ce2154ce23c24e
Ahs5089 <140600887+Ahs5089@users.noreply.github.com>
2023-09-14 12:39:06 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,9 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
-#from prediction import predict
+import pickle
 
-st.title('Game Store Sales Predict')
-# st.markdown('Toy model to play to classify iris flowers into \
-# setosa, versicolor, virginica’)
-
-# st.header("Plant Features”)
-# col1, col2 = st.columns(2)
-# with col1:
-# st.text('Sepal characteristics”)
-# sepal_l = st.slider('Sepal lenght (cm)’, 1.0, 8.0, 0.5)
-# sepal_w = st.slider('Sepal width (cm)’, 2.0, 4.4, 0.5)
-# with col2:
-# st.text(“Pepal characteristics”)
-# petal_l = st.slider('Petal lenght (cm)’, 1.0, 7.0, 0.5)
-# petal_w = st.slider('Petal width (cm)’, 0.1, 2.5, 0.5)
-
-# st.button('Predict type of Iris”)
+# Load the model
+model_path = "finalized_model.pkl"
+with open (model_path, "rb") as model_file:
+  model = pickle.load(model_file)
